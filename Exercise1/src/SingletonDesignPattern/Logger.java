48fd4379445edac309861f32e8c20cc993425ea0
kruti2924 <krutidharsandiya2924@gmail.com>
2025-09-27 21:30:26 +0530

    Adding comments

--- Exercise1/src/SingletonDesignPattern/Logger.java
+++ Exercise1/src/SingletonDesignPattern/Logger.java
@@ -1,10 +1,14 @@
 package SingletonDesignPattern;
 
 public class Logger {
+    //private static instance
     private static Logger instance;
+
+    //private constructor
     private Logger() {}
 
     public static Logger getInstance() {
+        //lazy initialization
         if (instance == null) {
             instance = new Logger();
         }
